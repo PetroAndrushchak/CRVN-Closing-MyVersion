998486037f46ad1ba8e2d41bb97df240de6db57d
Petro Andrushchak <pandrushchak@ukeess.com>
2023-01-24 23:08:24 +0200

    Redux Saga

--- src/store/store.js
+++ src/store/store.js
@@ -4,10 +4,12 @@ import logger from 'redux-logger';
 
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import thunk from 'redux-thunk';
 
+import createSagaMiddleware from '@redux-saga/core';
+import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
+
 
 const persistConfig = {
   key: 'root',
@@ -23,10 +25,11 @@ const composeEnhancer =
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
+const sagaMiddleware = createSagaMiddleware();
 
 const middleWares = [
   process.env.NODE_ENV === 'development' && logger,
-  thunk
+  sagaMiddleware
 ].filter(
   Boolean
 );
@@ -34,6 +37,8 @@ const middleWares = [
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
 
